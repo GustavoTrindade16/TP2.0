1c1fb65b76c9cf221e257f806e42ca7293b085ee
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-23 21:50:29 +0000

    Adicionei a documentação do ficheiro io_terminal-pause.py

--- Programa/io_terminal-pause.py
+++ Programa/io_terminal-pause.py
@@ -1,4 +1,27 @@
 def pause():
-    """Faz uma pausa no programa e espera que o utilizador pressione ENTER"""
+    """
+    Faz uma pausa no programa e aguarda que o utilizador pressione ENTER.
 
+    Esta função é utilizada para criar uma pausa durante a execução do programa,
+    permitindo que o utilizador pressione a tecla ENTER para continuar a execução.
+
+    Args:
+        Nenhum.
+
+    Returns:
+        None
+
+    Exemplo:
+        >>> pause()
+        Pressione ENTER para continuar...
+
+    Nota:
+        Esta função utiliza a função `input` do Python para aguardar a entrada do utilizador.
+        Após a pausa, o programa continuará a execução.
+
+    Tarefas Pendentes:
+        - Considerar a possibilidade de adicionar uma mensagem personalizada.
+
+    """
     input("Pressione ENTER para continuar...")
+
