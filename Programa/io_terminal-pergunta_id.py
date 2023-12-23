dc813b5ed1316ace40cbff5a275bc8a065301b7b
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-23 21:57:33 +0000

    Adicionei a documentação do ficheiro io_terminal-pergunta_idpy

--- Programa/io_terminal-pergunta_id.py
+++ Programa/io_terminal-pergunta_id.py
@@ -1,12 +1,46 @@
 def pergunta_id(questao, lista, mostra_lista=False):
-    """TODO: documentação
-
-    :param questao:
-    :param lista:
-    :param mostra_lista:
-    :return:
     """
+    Solicita ao utilizador um ID da lista.
 
+    Esta função interage com o utilizador, apresentando uma questão e, opcionalmente,
+    mostrando a lista de opções disponíveis. O utilizador é solicitado a inserir um ID,
+    e a função valida se o ID está dentro dos limites da lista. Se não estiver, o utilizador
+    é informado, e a pergunta é repetida até que um ID válido seja fornecido.
+
+    Args:
+        questao (str): A questão a ser apresentada ao utilizador.
+        lista (list): A lista de opções disponíveis.
+        mostra_lista (bool): Se True, imprime a lista antes de solicitar o ID.
+
+    Returns:
+        int: O ID fornecido pelo utilizador.
+
+    Example:
+        >>> opcoes = ["Opção A", "Opção B", "Opção C"]
+        >>> pergunta_id("Escolha uma opção:", opcoes, mostra_lista=True)
+        Escolha uma opção:
+        +----+----------+
+        | id | Opções   |
+        +====+==========+
+        | 0  | Opção A  |
+        | 1  | Opção B  |
+        | 2  | Opção C  |
+        +----+----------+
+        1
+
+    Nota:
+        Se a opção `mostra_lista` for True, a função utiliza a função `imprime_lista` para
+        apresentar as opções disponíveis.
+
+    Tarefas Pendentes:
+        - Adicionar suporte a mensagens de erro personalizadas.
+
+    :param questao: A questão a ser apresentada ao utilizador.
+    :param lista: A lista de opções disponíveis.
+    :param mostra_lista: Se True, imprime a lista antes de solicitar o ID.
+    :return: O ID fornecido pelo utilizador.
+    """
+    
     if mostra_lista:
         imprime_lista(cabecalho="", lista=lista)
 
@@ -15,4 +49,5 @@ def pergunta_id(questao, lista, mostra_lista=False):
         if 0 <= id < len(lista):
             return id
         else:
-            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
+            print(f"ID inexistente. Tente novamente. Valores admitidos de {0} a {len(lista) - 1}")
+
