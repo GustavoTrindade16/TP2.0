642a19de3db478be3b7a206c794ce039a1e46e97
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-23 21:45:07 +0000

    Adicionei a documentação do ficheiro io_terminal-imprime_lista.py

--- Programa/io_terminal-imprime_lista.py
+++ Programa/io_terminal-imprime_lista.py
@@ -1,6 +1,13 @@
 def imprime_lista(cabecalho, lista):
     """Imprime a :attr:`lista` na forma de uma tabela com um cabeçalho
 
+    Esta função recebe uma lista de dicionários e imprime no terminal uma tabela
+    com um cabeçalho especificado. A tabela é formatada utilizando a biblioteca tabulate.
+
+    Argumentos:
+        cabecalho (str): Cabeçalho para a listagem.
+        lista (list): Lista de dicionários a ser impressa.
+
     Recebe uma lista na forma [{"atrib1": valor1, "atrib2": valor2, ...},
     {"atrib1": valor1, "atrib2": valor2, ...}, ...] e imprime no terminal uma tabela
     na forma
@@ -11,11 +18,33 @@ def imprime_lista(cabecalho, lista):
     0   valor1  valor2
     1   ...      ...
     ==  ======  ======
+    
+    Exemplo:
+
+        >>> cabecalho = "Listagem de Clientes"
+        >>> lista = [{"id": 0, "nome": "João", "idade": 25},
+        ...          {"id": 1, "nome": "Maria", "idade": 30}]
+        >>> imprime_lista(cabecalho, lista)
+        +----+-------+-------+
+        | id | nome  | idade |
+        +====+=======+=======+
+        | 0  | João  | 25    |
+        | 1  | Maria | 30    |
+        +----+-------+-------+
 
     :param cabecalho: Cabecalho para a listagem
     :param lista: Lista a ser impressa
-    """
 
+    Returns:
+        None
+
+    Verificar:
+        ValueError: Se a lista estiver vazia.
+
+    Notação:
+        Se a lista estiver vazia, será levantada uma exceção ValueError.
+    """
+    
     print(cabecalho)
 
     if (len(lista) == 0):
@@ -27,3 +56,8 @@ def imprime_lista(cabecalho, lista):
         lista_a_imprimir.extend([[id] + list(d.values()) for id, d in enumerate(lista)])
 
         print(tabulate(lista_a_imprimir, headers="firstrow", tablefmt='psql'))
+
+
+        from tabulate import tabulate
+
+
