8332f07c6a05834ede90060a1acc6a436af5cbbd
GustavoTrindade16 <a85492@ualg.pt>
2023-12-23 21:34:56 +0000

    implementação da documentação no io_ficheiros-carrega_as_listas_dos_ficheiros.py para melhorar o intendimento

--- Programa/io_ficheiros-carrega_as_listas_dos_ficheiros.py
+++ Programa/io_ficheiros-carrega_as_listas_dos_ficheiros.py
@@ -1,5 +1,14 @@
 def carrega_as_listas_dos_ficheiros():
-    """TODO: documentação"""
+     """
+    Carrega as listas de veículos, clientes e faturas a partir dos respetivos ficheiros.
+
+    Esta função lê os dados armazenados nos ficheiros correspondentes e retorna as listas
+    de veículos, clientes e faturas. Cada lista é carregada de acordo com o formato de
+    serialização utilizado.
+
+    :return: Uma tupla contendo as listas carregadas (lista_de_veiculos, lista_de_clientes, lista_de_faturas).
+    :rtype: tuple
+    """
 
     lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_veiculos)
     lista_de_clientes = le_de_ficheiro(nome_ficheiro_lista_de_clientes)
