a5d34188b6ee43b665c1c5c41faa7cd3477dff19
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-23 21:54:50 +0000

    Adicionei o ficheiro io_terminal-pause_lista.py para o ficheiro io_terminal.py

--- Programa/io_terminal.py
+++ Programa/io_terminal.py
@@ -62,6 +62,33 @@ def imprime_lista(cabecalho, lista):
 
         from tabulate import tabulate
 
+def pause():
+    """
+    Faz uma pausa no programa e aguarda que o utilizador pressione ENTER.
+
+    Esta função é utilizada para criar uma pausa durante a execução do programa,
+    permitindo que o utilizador pressione a tecla ENTER para continuar a execução.
+
+    Args:
+        Nenhum.
+
+    Returns:
+        None
+
+    Exemplo:
+        >>> pause()
+        Pressione ENTER para continuar...
+
+    Nota:
+        Esta função utiliza a função `input` do Python para aguardar a entrada do utilizador.
+        Após a pausa, o programa continuará a execução.
+
+    Tarefas Pendentes:
+        - Considerar a possibilidade de adicionar uma mensagem personalizada.
+
+    """
+    input("Pressione ENTER para continuar...")
+
 
 # TODO: Copie para aqui o código de cada uma das funções nos
 # ficheiros com o nome io_terminal*.py e faça um commit de cada vez
