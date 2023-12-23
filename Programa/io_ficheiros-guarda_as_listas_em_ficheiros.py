83ed8d2e441dc34be674963b399fe817d2cacef1
GustavoTrindade16 <a85492@ualg.pt>
2023-12-23 21:29:19 +0000

    melhorando a documentação do io_ficheiros-guarda_as_listas_em_ficheiros

--- Programa/io_ficheiros-guarda_as_listas_em_ficheiros.py
+++ Programa/io_ficheiros-guarda_as_listas_em_ficheiros.py
@@ -1,9 +1,14 @@
 def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas):
-    """TODO: documentação
+   """
+    Guarda as listas de veículos, clientes e faturas em ficheiros.
 
-    :param lista_de_clientes:
-    :param lista_de_veiculos:
-    :param lista_de_faturas:
+    :param lista_de_veiculos: Lista de veículos a ser guardada no ficheiro.
+    :type lista_de_veiculos: list
+    :param lista_de_clientes: Lista de clientes a ser guardada no ficheiro.
+    :type lista_de_clientes: list
+    :param lista_de_faturas: Lista de faturas a ser guardada no ficheiro.
+    :type lista_de_faturas: list
+    :return: None
     """
 
     op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
